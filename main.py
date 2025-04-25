12feec28e72e2f9cad65de70f341aa8aca416f7b
takato-taniguchi <takato.taniguchi@geniee.co.jp>
2025-04-25 10:05:51 +0900

    add: sidebar

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from bake-rows!")
+import streamlit as st
+import polars as pl
+from pathlib import Path
 
+files = []
 
-if __name__ == "__main__":
-    main()
+with st.sidebar:
+    for file in files:
+        st.write(file)
+
+    file = st.file_uploader("Upload CSV or Excel files", type=["csv", "xlsx", "xls"])
+    files.append(file)
