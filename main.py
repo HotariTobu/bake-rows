dc5c5d1d1359414d9a2aa30c9d56436726aea33d
takato-taniguchi <takato.taniguchi@geniee.co.jp>
2025-04-27 14:10:46 +0900

    ref: to access session_state via se

--- main.py
+++ main.py
@@ -2,28 +2,27 @@ import streamlit as st
 import polars as pl
 from pathlib import Path
 
-if "files" not in st.session_state:
-    st.session_state.files = {}
+se = st.session_state
 
-if "selected" not in st.session_state:
-    st.session_state.selected = None
+if "files" not in se:
+    se.files = {}
 
-files = st.session_state.files
-selected = st.session_state.selected
+if "selected" not in se:
+    se.selected = None
 
 with st.sidebar:
-    for id, file in files.items():
+    for id, file in se.files.items():
         if st.button(file.name, key=id):
-            st.session_state.selected = id
+            se.selected = id
             st.rerun()
 
     file = st.file_uploader("Upload CSV or Excel files", type=["csv", "xlsx", "xls"])
     if file is not None:
-        if file.file_id not in files:
-            st.session_state.files[file.file_id] = file
+        if file.file_id not in se.files:
+            se.files[file.file_id] = file
             st.rerun()
 
-if selected is None:
+if se.selected is None:
     exit()
 
-st.title(files[selected].name)
+st.title(se.files[se.selected].name)
