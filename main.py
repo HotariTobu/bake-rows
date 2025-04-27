d85ef4253238bd411ba6295ed93295bb36e0ade4
takato-taniguchi <takato.taniguchi@geniee.co.jp>
2025-04-27 14:06:33 +0900

    add: file select buttons

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ import streamlit as st
 import polars as pl
 from pathlib import Path
 
-files = []
+if "files" not in st.session_state:
+    st.session_state.files = {}
+
+if "selected" not in st.session_state:
+    st.session_state.selected = None
+
+files = st.session_state.files
+selected = st.session_state.selected
 
 with st.sidebar:
-    for file in files:
-        st.write(file)
+    for id, file in files.items():
+        if st.button(file.name, key=id):
+            st.session_state.selected = id
+            st.rerun()
 
     file = st.file_uploader("Upload CSV or Excel files", type=["csv", "xlsx", "xls"])
-    files.append(file)
+    if file is not None:
+        if file.file_id not in files:
+            st.session_state.files[file.file_id] = file
+            st.rerun()
