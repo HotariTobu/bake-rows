2417a8f2669fc4a6161a36a2f1255ec4244e0734
takato-taniguchi <takato.taniguchi@geniee.co.jp>
2025-05-01 20:12:20 +0900

    wip: gas

--- app/src/index.ts
+++ app/src/index.ts
@@ -14,3 +14,80 @@
  * limitations under the License.
  */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+
+const onOpen = (_: GoogleAppsScript.Events.SheetsOnOpen) => {
+  SpreadsheetApp.getUi()
+    .createMenu('BAKE ROWS')
+    .addItem('サイドバーを開く', '__showSidebar__')
+    .addToUi();
+};
+
+const __showSidebar__ = () => {
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet = ss.getActiveSheet();
+
+  const template = HtmlService.createTemplateFromFile('page');
+  template.sheetId = sheet.getSheetId();
+  template.columnsJSON = JSON.stringify(__extractColumns__(sheet));
+  const html = template.evaluate().setTitle('BAKE ROWS');
+
+  const ui = SpreadsheetApp.getUi();
+  ui.showSidebar(html);
+};
+
+const __extractColumns__ = (sheet: GoogleAppsScript.Spreadsheet.Sheet) => {
+  const headerRange = sheet.getRange('1:1');
+  const headerValues = headerRange.getValues();
+  const headers = headerValues[0];
+  const columns: [number, string][] = [];
+
+  for (let i = 0; i < headers.length; i++) {
+    const header = headers[i];
+    if (header) {
+      columns.push([i + 1, header]); // Store column index (1-based)
+    }
+  }
+  return columns;
+};
+
+const getColumnValuesRecord = (
+  sheetId: GoogleAppsScript.Integer,
+  columnIndexRecord: Record<string, GoogleAppsScript.Integer>
+) => {
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet = ss.getSheetById(sheetId);
+  if (sheet === null) {
+    throw new Error(`Not found: sheet ${sheetId}`);
+  }
+
+  const columnValuesRecord: Record<string, unknown[]> = {};
+
+  for (const name of Object.keys(columnIndexRecord)) {
+    const columnIndex = columnIndexRecord[name];
+    const columnValues = __getColumnValues__(sheet, columnIndex);
+    columnValuesRecord[name] = columnValues.map(value => String(value));
+  }
+
+  return columnValuesRecord;
+};
+
+const __getColumnValues__ = (
+  sheet: GoogleAppsScript.Spreadsheet.Sheet,
+  columnIndex: GoogleAppsScript.Integer
+) => {
+  const maxRow = sheet.getMaxRows();
+  const range = sheet.getRange(2, columnIndex, maxRow, columnIndex);
+  const columnValues = range.getValues().map(row => row[0]);
+  return columnValues;
+};
+
+const insertOutputSheet = () => {
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet = ss.insertSheet();
+
+  sheet.setTabColor('#FF0000');
+
+  sheet.getRange('A1').setValue('Hello Apps Script!');
+  sheet.setFrozenRows(1);
+  sheet.setFrozenColumns(1);
+};
